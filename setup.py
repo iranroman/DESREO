ef256b97a7874a9f7c21a3f6d4fac84240ab191f
Chen, Wayne <wayne.chen@dolby.com>
2023-08-08 14:58:29 -0700

    fix whitespace

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
         ],                                                                                                   
     install_requires=[
         'pathlib',
-		'pytube',
+        'pytube',
         'demucs',
     ],                                                                                                       
 )    
